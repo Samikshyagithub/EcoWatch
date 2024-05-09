1dc72d12439cf4779aca720939a2d78b779993a6
abiral-adhikari <109904208+abiral-adhikari@users.noreply.github.com>
2024-05-09 13:34:40 +0545

    prediction function defined by loaidng class ,setting color for the detection frame and reszie video frame

--- DumpDetect/app.py
+++ DumpDetect/app.py
@@ -16,3 +16,33 @@ app = Flask(__name__)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+def predict1(video_path): 
+    # opening the file in read mode
+    my_file = open("class.txt", "r")
+    # reading the file
+    data = my_file.read()
+    # replacing end splitting the text | when newline ('\n') is seen.
+    class_list = data.split("\n")
+    my_file.close()
+
+    
+    
+    # Generate random colors for class list
+    detection_colors = []
+    for i in range(len(class_list)):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        detection_colors.append((r, b, g))
+
+    # # load a pretrained YOLOv8n model
+    # model = YOLO("mydrive/results/train/weights/best.pt","v8")
+
+    # Vals to resize video frames | small frame optimise the run
+    frame_wid = 640
+    frame_hyt = 480
+    
+    cap = cv2.VideoCapture(video_path)
+    counter=0
+    carry_record=[0,0,0]
